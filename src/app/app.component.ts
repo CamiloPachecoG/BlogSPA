cc8e37fb8d3d88283ea05488002df3a8a3bec3a4
camilo pacheco <camilopacheco@MacBook-Air-de-camilo.local>
2019-12-26 12:37:59 -0300

    Categorias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,32 @@
 import { Component, OnInit, DoCheck } from '@angular/core';
 import { UserService } from './services/user.service';
+import { CategoryService } from './services/category.services';
+import { global } from './services/global';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [UserService]
+  providers: [UserService, CategoryService]
 })
 export class AppComponent implements OnInit, DoCheck {
   title = 'Blog en Angular';
   public identity;
   public token;
+  public url;
+  public categories;
 
   constructor(
-    public _userService: UserService
+    private _userService: UserService,
+    private _categoryService: CategoryService
   ){
     this.loadUser();
+    this.url = global.url;
   }
 
   ngOnInit(){
     console.log('Web Cargada');
+    this.getCategories();
   }
 
   ngDoCheck(){
@@ -29,6 +36,20 @@ export class AppComponent implements OnInit, DoCheck {
   loadUser(){
     this.identity = this._userService.getIdentity();
     this.token = this._userService.getToken();
+  }
+
+  getCategories(){
+    this._categoryService.getCategories().subscribe(
+      response => {
+        if(response.status == 'success'){
+          this.categories = response.categories;
+          console.log(this.categories);
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
 }
